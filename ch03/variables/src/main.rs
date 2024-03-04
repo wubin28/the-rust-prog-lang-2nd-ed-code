752a32b98bb678d45caff8bfa531145d987604b6
Ben Wu <wubin28@gmail.com>
2024-03-04 21:54:22 +0800

    feat: ch03: invalid array element access

--- ch03/variables/src/main.rs
+++ ch03/variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     // variables are immutable by default
     // let x = 5;
@@ -46,4 +48,24 @@ fn main() {
 
     let two = x.2;
     println!("The value of x.1 is: {}", x.1);
+
+    // invalid array element access
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
