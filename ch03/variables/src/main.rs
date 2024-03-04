2a9a8d972e854c4341a132c4353cad1c7a395712
Ben Wu <wubin28@gmail.com>
2024-03-04 21:05:26 +0800

    feat: ch03: tuple destructuring

--- ch03/variables/src/main.rs
+++ ch03/variables/src/main.rs
@@ -29,4 +29,12 @@ fn main() {
     let spaces = "   ";
     let mut spaces = spaces.len();
     // spaces = "1"; // expected `usize`, but found `&str`
+
+    // The tuple type
+    //let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
 }
